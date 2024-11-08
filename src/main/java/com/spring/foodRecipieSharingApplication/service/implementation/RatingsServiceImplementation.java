8ed39cfca0d27f763b07986b1fd1a2c4033ce292
sunaina <sunaina11sadare@gmail.com>
2024-11-08 18:37:35 +0530

    Ratings entity is added

---
+++ src/main/java/com/spring/foodRecipieSharingApplication/service/implementation/RatingsServiceImplementation.java
@@ -0,0 +1,8 @@
+package com.spring.foodRecipieSharingApplication.service.implementation;
+
+import com.spring.foodRecipieSharingApplication.service.RatingsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingsServiceImplementation implements RatingsService{
+}
