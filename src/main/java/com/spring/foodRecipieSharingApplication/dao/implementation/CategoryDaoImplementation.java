a5d2a67deef9300588577be8c1de2b33ee00a877
Gowthami MC <gowthamimc@Gowthamis-MacBook-Air.local>
2024-11-08 18:27:40 +0530

    Category entity is added

---
+++ src/main/java/com/spring/foodRecipieSharingApplication/dao/implementation/CategoryDaoImplementation.java
@@ -0,0 +1,8 @@
+package com.spring.foodRecipieSharingApplication.dao.implementation;
+
+import com.spring.foodRecipieSharingApplication.dao.CategoryDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CategoryDaoImplementation implements CategoryDao{
+}
---
+++ src/main/java/com/spring/foodRecipieSharingApplication/models/dto/CategoryDto.java
@@ -0,0 +1,8 @@
+package com.spring.foodRecipieSharingApplication.models.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDto {
+    private String name;
+}
