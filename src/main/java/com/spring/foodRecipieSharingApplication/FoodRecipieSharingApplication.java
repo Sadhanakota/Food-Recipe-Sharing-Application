0413448092bf4c2570f2a9c98ad57de018cd8318
Sadhana <kotasadhna@gmail.com>
2024-11-08 15:34:47 +0530

    Food Recipe Project Structure Commited

---
+++ src/main/java/com/spring/foodRecipieSharingApplication/FoodRecipieSharingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.foodRecipieSharingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipieSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipieSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/foodRecipieSharingApplication/FoodRecipieSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.foodRecipieSharingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipieSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
