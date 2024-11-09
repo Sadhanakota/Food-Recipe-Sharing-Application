98a3c3ba7251cfa8dcd01d6eb3e0ddea43463df4
Maheswari <kmahesh1602@gmail.com>
2024-11-09 13:41:33 +0530

    RecipeDao amd DaoImplementation are modified

--- src/main/java/com/spring/foodRecipieSharingApplication/dao/RecipeDao.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/dao/RecipeDao.java
@@ -1,4 +1,15 @@
 package com.spring.foodRecipieSharingApplication.dao;
 
+import com.spring.foodRecipieSharingApplication.models.entity.Recipe;
+
+import java.util.List;
+
 public interface RecipeDao {
+
+    Recipe createRecipe(Recipe recipe);
+    Recipe findRecipeById(int id);
+    Recipe updateRecipe(Recipe recipe);
+    String deleteRecipe(Recipe recipe);
+    List<Recipe> getAllRecipes();
+
 }
