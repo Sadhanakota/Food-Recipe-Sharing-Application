5885768839763f188a4a9939d40f7938ed41bfb1
sunaina <sunaina11sadare@gmail.com>
2024-11-09 13:33:44 +0530

    Ratings Dao and DaoImplementation is modified

--- src/main/java/com/spring/foodRecipieSharingApplication/dao/implementation/RatingsDaoImplementation.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/dao/implementation/RatingsDaoImplementation.java
@@ -1,9 +1,48 @@
 package com.spring.foodRecipieSharingApplication.dao.implementation;
 
 import com.spring.foodRecipieSharingApplication.dao.RatingsDao;
+import com.spring.foodRecipieSharingApplication.models.entity.Ratings;
+import com.spring.foodRecipieSharingApplication.repository.RatingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public class RatingsDaoImplementation implements RatingsDao {
+
+    @Autowired
+    private RatingsRepository ratingsRepository;
+
+    @Override
+    public Ratings createRatings(Ratings ratings) {
+        return ratingsRepository.save(ratings);
+    }
+    @Override
+    public Ratings findRatingsById(int id){
+        Optional ratings = ratingsRepository.findById(id);
+        if (ratings.isPresent()) {
+            return (Ratings) ratings.get();
+        }
+        else {
+            return null;
+        }
+
+    }
+    @Override
+    public Ratings updateRatings(Ratings ratings){
+        return ratingsRepository.save(ratings);
+    }
+    @Override
+    public String deleteRatings(Ratings ratings){
+        ratingsRepository.delete(ratings);
+        return "Ratings are deleted" ;
+    }
+    @Override
+    public List<Ratings> getAllRatingsList(){
+        return ratingsRepository.findAll();
+    }
+
 }
