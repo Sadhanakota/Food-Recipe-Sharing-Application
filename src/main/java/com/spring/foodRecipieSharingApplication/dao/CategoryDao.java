24ee886a68181801951af62e54dfeb22f6980a74
Gowthami MC <gowthamimc@Gowthamis-MacBook-Air.local>
2024-11-09 13:20:58 +0530

    Category Dao and implementation is modified

--- src/main/java/com/spring/foodRecipieSharingApplication/dao/CategoryDao.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/dao/CategoryDao.java
@@ -1,4 +1,13 @@
 package com.spring.foodRecipieSharingApplication.dao;
 
+import com.spring.foodRecipieSharingApplication.models.entity.Category;
+
+import java.util.List;
+
 public interface CategoryDao {
+    Category createCategory(Category category);
+    Category findCategoryById(int id);
+    Category updateCategory(Category category);
+    String deleteCategory(Category category);
+    List<Category> getAllCategoryList();
 }
--- src/main/java/com/spring/foodRecipieSharingApplication/dao/implementation/CategoryDaoImplementation.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/dao/implementation/CategoryDaoImplementation.java
@@ -1,8 +1,47 @@
 package com.spring.foodRecipieSharingApplication.dao.implementation;
 
 import com.spring.foodRecipieSharingApplication.dao.CategoryDao;
+import com.spring.foodRecipieSharingApplication.models.entity.Category;
+import com.spring.foodRecipieSharingApplication.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CategoryDaoImplementation implements CategoryDao{
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Category createCategory(Category category){
+        return categoryRepository.save(category);
+    }
+    @Override
+    public Category findCategoryById(int id){
+        Optional category = categoryRepository.findById(id);
+        if (category.isPresent()){
+            return (Category) category.get();
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public Category updateCategory(Category category){
+        return categoryRepository.save(category);
+    }
+    @Override
+    public String deleteCategory(Category category){
+        categoryRepository.delete(category);
+        return "Category is deleted";
+    }
+    @Override
+    public List<Category> getAllCategoryList(){
+        return categoryRepository.findAll();
+    }
+
+
+
 }
