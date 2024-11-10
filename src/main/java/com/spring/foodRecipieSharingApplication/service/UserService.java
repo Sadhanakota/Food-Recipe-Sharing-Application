75cc15480b17e668605a5f0781296f49f703f921
Jagruti <jagrutimohanty12@gmail.com>
2024-11-10 12:02:22 +0530

    user service, userserviceimp and controller modfied

--- src/main/java/com/spring/foodRecipieSharingApplication/service/UserService.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/service/UserService.java
@@ -5,7 +5,21 @@ import com.spring.foodRecipieSharingApplication.models.dto.UserDto;
 import com.spring.foodRecipieSharingApplication.models.entity.User;
 import org.springframework.http.ResponseEntity;
 
-public interface UserService {
+import java.util.List;
 
+public interface UserService {
+//Save
 public ResponseEntity<ResponseStructure<User>> createUser(UserDto userDto);
+
+    //Find
+   public ResponseEntity<ResponseStructure<User>> findUserById(int id);
+
+   //Update
+    public ResponseEntity<ResponseStructure<User>> updateUser(int id,UserDto userDto);
+
+    //Delete
+    ResponseEntity<ResponseStructure<String>> deleteUser(int id);
+
+    //FindAll
+    ResponseEntity<ResponseStructure<List<User>>> getAllUsers();
 }
--- src/main/java/com/spring/foodRecipieSharingApplication/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/spring/foodRecipieSharingApplication/service/implementation/UserServiceImplementation.java
@@ -1,15 +1,18 @@
 package com.spring.foodRecipieSharingApplication.service.implementation;
 
 import com.spring.foodRecipieSharingApplication.dao.UserDao;
+import com.spring.foodRecipieSharingApplication.exception.DataNotFoundException;
 import com.spring.foodRecipieSharingApplication.models.dto.ResponseStructure;
 import com.spring.foodRecipieSharingApplication.models.dto.UserDto;
 import com.spring.foodRecipieSharingApplication.models.entity.User;
-import com.spring.foodRecipieSharingApplication.service.RecipeService;
 import com.spring.foodRecipieSharingApplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService {
@@ -20,7 +23,6 @@ public class UserServiceImplementation implements UserService {
     public ResponseEntity<ResponseStructure<User>> createUser(UserDto userDto) {
         User user = new User();
         user.setUserName(userDto.getUserName());
-
         user.setUserEmail(userDto.getUserEmail());
         user.setUserPlace(userDto.getUserPlace());
 
@@ -31,6 +33,94 @@ public class UserServiceImplementation implements UserService {
         return new ResponseEntity<>(responseStructure,HttpStatus.CREATED);
 
     }
+
+
+    //Find
+    @Override
+    public ResponseEntity<ResponseStructure<User>> findUserById(int id) {
+        ResponseStructure<User> responseStructure = new ResponseStructure<User>();
+        User user = userDao.findUserById(id);
+        if (user != null) {
+            responseStructure.setData(user);
+            responseStructure.setMessage("User found successfully");
+            responseStructure.setStatusCode(HttpStatus.FOUND.value());
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        }
+        else {
+            throw new DataNotFoundException("User not found");
+        }
+
+    }
+//Update
+    @Override
+    public ResponseEntity<ResponseStructure<User>> updateUser(int id,UserDto userDto) {
+        ResponseStructure<User> responseStructure = new ResponseStructure<User>();
+        User existingUser = userDao.findUserById(id);
+        if (existingUser != null) {
+
+            if (userDto.getUserName() != null) {
+                existingUser.setUserName(userDto.getUserName());
+            }
+            if (userDto.getUserEmail() != null) {
+                existingUser.setUserEmail(userDto.getUserEmail());
+            }
+            if (userDto.getUserPlace() != null) {
+                existingUser.setUserPlace(userDto.getUserPlace());
+            }
+
+            // Save the updated user
+            User updatedUser = userDao.updateUser(existingUser);
+
+
+            responseStructure.setData(updatedUser);
+            responseStructure.setMessage("User updated successfully");
+            responseStructure.setStatusCode(HttpStatus.OK.value());
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        } else {
+            throw new DataNotFoundException("User not found");
+        }
+    }
+    //Delete
+    @Override
+    public ResponseEntity<ResponseStructure<String>> deleteUser(int id) {
+        ResponseStructure<String> responseStructure = new ResponseStructure<>();
+        User existingUser = userDao.findUserById(id);
+
+        if (existingUser != null) {
+        String deleteUser = userDao.deleteUser(existingUser);
+
+            responseStructure.setData(deleteUser);
+            responseStructure.setMessage("Deleted successfully");
+            responseStructure.setStatusCode(HttpStatus.OK.value());
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        } else {
+            throw new DataNotFoundException("User not found");
+        }
+
+    }
+
+    //FindAll
+    @Override
+    public ResponseEntity<ResponseStructure<List<User>>> getAllUsers() {
+        List<User> userList = userDao.getAllUser();
+
+        ResponseStructure<List<User>> responseStructure = new ResponseStructure<>();
+        if (userList != null && !userList.isEmpty()) {
+            responseStructure.setData(userList);
+            responseStructure.setMessage("Users retrieved successfully");
+            responseStructure.setStatusCode(HttpStatus.OK.value());
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        } else {
+            responseStructure.setData(new ArrayList<>());
+            responseStructure.setMessage("No users found");
+            responseStructure.setStatusCode(HttpStatus.OK.value());
+            return new ResponseEntity<>(responseStructure, HttpStatus.OK);
+        }
+    }
+
+
+
+
 
 
 
